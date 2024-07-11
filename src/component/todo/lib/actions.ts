031e104e1bf543a9faaf230fdd2590b65bba48df
jowix <jowix311@gm,ail.com>
2024-07-11 16:19:48 +0800

    Added error handling

--- src/component/todo/lib/actions.ts
+++ src/component/todo/lib/actions.ts
@@ -4,34 +4,49 @@ import prisma from "@lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export const addTodo = async () => {
-  const newEntry = {
-    title: "Todo - This just statically generated",
-    completed: false,
-  };
-  await prisma.todo.create({ data: newEntry });
-
+  try {
+    const newEntry = {
+      title: "Todo - This just statically generated",
+      completed: false,
+    };
+    await prisma.todo.create({ data: newEntry });
+  } catch (error) {
+    console.error("Error creating todo:", error);
+    throw new Error("Failed to create todo");
+  }
   revalidatePath("/");
 };
 
 export const deleteTodo = async (id: string) => {
-  await prisma.todo.delete({
-    where: {
-      id,
-    },
-  });
-
+  try {
+    await prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+    throw new Error("Failed to delete todo");
+  }
   revalidatePath("/");
 };
 
-export const updateCompletionStatus = async (id: string, isCompleted: boolean) => {
-  await prisma.todo.update({
-    where: {
-      id,
-    },
-    data: {
-      completed: isCompleted,
-    },
-  });
-
+export const updateCompletionStatus = async (
+  id: string,
+  isCompleted: boolean,
+) => {
+  try {
+    await prisma.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        completed: isCompleted,
+      },
+    });
+  } catch (error) {
+    console.error("Error updating todo:", error);
+    throw new Error("Failed to update todo");
+  }
   revalidatePath("/");
 };
