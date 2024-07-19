70ba61ab23d49cb279153575825f80ff67abaa62
jowix <jowix311@gm,ail.com>
2024-07-19 19:38:03 +0800

    added font and color palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-josefin-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={josefinSans.className}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          brightBlue: 'hsl(220, 98%, 61%)',
+        },
+        checkBackground: 'linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))',
+        neutral: {
+          lightTheme: {
+            veryLightGray: 'hsl(0, 0%, 98%)',
+            veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+            lightGrayishBlue: 'hsl(233, 11%, 84%)',
+            darkGrayishBlue: 'hsl(236, 9%, 61%)',
+            veryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+          },
+          darkTheme: {
+            veryDarkBlue: 'hsl(235, 21%, 11%)',
+            veryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+            lightGrayishBlue: 'hsl(234, 39%, 85%)',
+            lightGrayishBlueHover: 'hsl(236, 33%, 92%)',
+            darkGrayishBlue: 'hsl(234, 11%, 52%)',
+            veryDarkGrayishBlue: {
+              DEFAULT: 'hsl(233, 14%, 35%)',
+              secondary: 'hsl(237, 14%, 26%)',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [],
