9836b44c7b22a53b6ab23187dc6170089cca0a21
jowi <silkherb@gmail.com>
2024-08-06 21:53:00 +0800

    added gradient on button initial logic

--- src/features/todo/todo-item.tsx/todo-item.tsx
+++ src/features/todo/todo-item.tsx/todo-item.tsx
@@ -2,9 +2,10 @@
 
 import { Button } from "@/components/ui/button";
 import clsx from "clsx";
-import { createContext, use, useContext, useEffect, useRef, useState } from "react";
-import { isContext } from "vm";
-import { create, createStore } from "zustand";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { createStore } from "zustand";
+import IconCheck from "@public/icon-check.svg";
+import Image from "next/image";
 
 // 1. Creating the Zustand store
 // Official Docs: https://github.com/pmndrs/zustand/blob/main/docs/guides/initialize-state-with-props.md
@@ -20,7 +21,6 @@ interface TodoItemState extends TodoItemProps {
 type TodoItemStore = ReturnType<typeof createTodoItemStore>;
 
 const createTodoItemStore = (initProps?: Partial<TodoItemProps>) => {
-  console.log("initProps", initProps);
   const DEFAULT_STATE = {
     title: "",
     isCompleted: false,
@@ -29,7 +29,8 @@ const createTodoItemStore = (initProps?: Partial<TodoItemProps>) => {
     ...DEFAULT_STATE,
     ...initProps,
     setTitle: (title) => set({ title }),
-    toggleCompleted: () => set((state) => ({ isCompleted: !state.isCompleted })),
+    toggleCompleted: () =>
+      set((state) => ({ isCompleted: !state.isCompleted })),
   }));
 };
 
@@ -67,33 +68,57 @@ export const useTodoItemContext = () => {
 // 5. Using it on the parent Compound Component
 type Props = {
   children: React.ReactNode;
-  prop: TodoItemProps
+  prop: TodoItemProps;
 };
 
 export const TodoItem = ({ children, prop }: Props) => {
   // return <TodoItemProvider {...prop}>{children}</TodoItemProvider>; // Keeping this code for reference purposes.
-  return <TodoItemProvider title={prop.title} isCompleted={true}>{children}</TodoItemProvider>;
+  return (
+    <TodoItemProvider title={prop.title} isCompleted={true}>
+      {children}
+    </TodoItemProvider>
+  );
 };
 
 const TodoItemIndicator = () => {
   const toggleCompleted = useTodoItemContext().getState().toggleCompleted;
+  const isCompleted = useTodoItemContext().getState().isCompleted;
 
   return (
-    <Button className="h-8 w-8 rounded-full border-[1px] border-blue-300" onClick={toggleCompleted}>
+    <Button
+      className={clsx(
+        "flex items-center justify-center h-8 w-8 rounded-full border-[1px] border-blue-300",
+        {
+          "bg-gradient-to-br from-blue-200 via-violet-400 to-violet-700":
+            isCompleted,
+        },
+      )}
+      onClick={toggleCompleted}
+    >
+      {isCompleted && (
+        <Image
+          className="flex"
+          src={IconCheck}
+          alt="icon check"
+          width={16}
+          height={16}
+        />
+      )}
     </Button>
   );
 };
 
 const TodoItemTitle = () => {
   const title = useTodoItemContext().getState().title;
-  const [isCompleted, setIsCompleted] = useState(useTodoItemContext().getState().isCompleted);
-
+  const [isCompleted, setIsCompleted] = useState(
+    useTodoItemContext().getState().isCompleted,
+  );
 
   const todoItemContext = useTodoItemContext();
 
   useEffect(() => {
     const unSubscribe = todoItemContext.subscribe((state, prevState) => {
-      if(state.isCompleted !== prevState.isCompleted) {
+      if (state.isCompleted !== prevState.isCompleted) {
         setIsCompleted(state.isCompleted);
       }
     });
@@ -101,9 +126,15 @@ const TodoItemTitle = () => {
     return unSubscribe;
   }, [todoItemContext, isCompleted]);
 
-  return <p className={clsx("text-white", {
-    "line-through": isCompleted,
-  })}>{title}</p>;
+  return (
+    <p
+      className={clsx("text-white", {
+        "line-through": isCompleted,
+      })}
+    >
+      {title}
+    </p>
+  );
 };
 
 TodoItem.Title = TodoItemTitle;
